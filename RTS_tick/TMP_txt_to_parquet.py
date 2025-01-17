14fb575cff8b07167330b60992c3e9beb01585a8
Aleksey Manin <Alkor135@gmail.com>
2025-01-17 15:50:25 +0300

    update TMP_txt_to_parquet.py

--- RTS_tick/TMP_txt_to_parquet.py
+++ RTS_tick/TMP_txt_to_parquet.py
@@ -11,8 +11,8 @@ print(df)
 # Сохранение DataFrame в файл .parquet
 df.to_parquet(r'C:\Users\Alkor\gd\data_ticks_finam\RTS_240905_240905.parquet')
 
-# Чтение файла
+# Чтение файла .parquet
 df_parquet = pd.read_parquet(r'C:\Users\Alkor\gd\data_ticks_finam\RTS_240905_240905.parquet')
 
-# Вывод первых 5 строк
+# Вывод
 print(df_parquet)
